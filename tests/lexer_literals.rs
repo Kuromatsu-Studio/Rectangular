24227a53dd90aab59e0d4ec53a868cf0bc2035db
KentoWada <kentowada333@gmail.com>
2026-09-15 16:53:45 +0300

    Added tests for the u32, i8, u8

--- tests/lexer_literals.rs
+++ tests/lexer_literals.rs
@@ -37,12 +37,45 @@ fn int_literal_with_underscore(){
 }
 
 #[test]
+fn int_literal_i8_suffix(){
+    let diag = make_diag("8i8");
+    let mut lexer = make_lexer("8i8", diag);
+    let tokens = lexer.tokenize();
+
+    assert_eq!(tokens[0].token_type, TType::I8Literal);
+    assert_eq!(tokens[0].lexeme, "8");
+    assert_eq!(tokens[1].token_type, TType::End);
+}
+
+#[test]
+fn int_literal_u8_suffix(){
+    let diag = make_diag("8u8");
+    let mut lexer = make_lexer("8u8", diag);
+    let tokens = lexer.tokenize();
+
+    assert_eq!(tokens[0].token_type, TType::U8Literal);
+    assert_eq!(tokens[0].lexeme, "8");
+    assert_eq!(tokens[1].token_type, TType::End);
+}
+
+#[test]
 fn int_literal_i32_suffix(){
-    let diag = make_diag("8i32");
-    let mut lexer = make_lexer("8i32", diag);
+    let diag = make_diag("32i32");
+    let mut lexer = make_lexer("32i32", diag);
     let tokens = lexer.tokenize();
 
     assert_eq!(tokens[0].token_type, TType::I32Literal);
-    assert_eq!(tokens[0].lexeme, "8");
+    assert_eq!(tokens[0].lexeme, "32");
+    assert_eq!(tokens[1].token_type, TType::End);
+}
+
+#[test]
+fn int_literal_u32_suffix(){
+    let diag = make_diag("32u32");
+    let mut lexer = make_lexer("32u32", diag);
+    let tokens = lexer.tokenize();
+    
+    assert_eq!(tokens[0].token_type, TType::U32Literal);
+    assert_eq!(tokens[0].lexeme, "32");
     assert_eq!(tokens[1].token_type, TType::End);
 }
