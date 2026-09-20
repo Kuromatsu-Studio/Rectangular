519c0c2006d53190c2359590cc7dc69b297407e1
KentoWada <kentowada333@gmail.com>
2026-09-20 12:48:05 +0300

    Added a test for the multi line comment

--- tests/lexer_whitespace_comments.rs
+++ tests/lexer_whitespace_comments.rs
@@ -13,3 +13,14 @@ fn single_line_comment(){
     assert_eq!(tokens[0].lexeme, "+");
     assert_eq!(tokens[1].token_type, TType::End);
 }
+
+#[test]
+fn multi_line_comment(){
+    let diag = make_diag("##This is a multi_line comment##\n+");
+    let mut lexer = make_lexer("##This is a  multi_line_comment##\n+", diag);
+    let tokens = lexer.tokenize();
+
+    assert_eq!(tokens[0].token_type, TType::Plus);
+    assert_eq!(tokens[0].lexeme, "+");
+    assert_eq!(tokens[1].token_type, TType::End);
+}
