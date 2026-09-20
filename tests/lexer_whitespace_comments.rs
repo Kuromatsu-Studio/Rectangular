8e9832f42e88f255d1b43fa63c22ca0669896c9b
KentoWada <kentowada333@gmail.com>
2026-09-20 13:02:15 +0300

    Added unterminated muti-line comment test

--- tests/lexer_whitespace_comments.rs
+++ tests/lexer_whitespace_comments.rs
@@ -17,10 +17,21 @@ fn single_line_comment(){
 #[test]
 fn multi_line_comment(){
     let diag = make_diag("##This is a multi_line comment##\n+");
-    let mut lexer = make_lexer("##This is a  multi_line_comment##\n+", diag);
+    let mut lexer = make_lexer("##This is a  multi_line comment##\n+", diag);
     let tokens = lexer.tokenize();
 
     assert_eq!(tokens[0].token_type, TType::Plus);
     assert_eq!(tokens[0].lexeme, "+");
     assert_eq!(tokens[1].token_type, TType::End);
+}
+
+#[test]
+fn unterminated_multi_line_comment(){
+    let diag = make_diag("## This is an unterminated muti_line comment ");
+    let mut lexer = make_lexer("## This is an unterminated multi_line comment ", diag.clone());
+    let tokens = lexer.tokenize();
+
+    assert_eq!(tokens.len(), 1);
+    assert_eq!(tokens[0].token_type, TType::End);
+    assert_eq!(diag.borrow().errors.len(), 1);
 }
